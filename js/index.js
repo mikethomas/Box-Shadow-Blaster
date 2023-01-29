6db296ef43baa21c0eba60eceb6c1e6de24832c6
Mike Thomas <chrisandsmith@gmail.com>
2023-01-29 15:15:44 -0600

    Laser loop created

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 const gameCanvas = document.querySelector('#game-canvas');
 const ctx = gameCanvas.getContext('2d');
 const playGameBtn = document.querySelector('.start-game');
-var playGame = false;
+let playGame = false;
+let lasers = [];
 
 const hero = {
   color: '#005717',
@@ -10,6 +11,18 @@ const hero = {
     y: 650,
     w: 50,
     h: 50
+  }
+}
+
+function Laser(xPos) {
+  return {
+    color: '#f00',
+    position: {
+      x: xPos,
+      y: 650,
+      w: 5,
+      h: 20
+    }
   }
 }
 
@@ -27,11 +40,24 @@ document.addEventListener('keydown', (e) => {
 function moveHero() {
   ctx.clearRect(0,0,1280,720);
   ctx.fillStyle = hero.color;
-  ctx.fillRect(hero.position.x, hero.position.y, hero.position.w, hero.position.h)
+  ctx.fillRect(hero.position.x, hero.position.y, hero.position.w, hero.position.h);
 };
 
+function moveLaser() {
+  for (var i = 0; i < lasers.length; i++) {
+    if (lasers[i].position.y < -10) {
+      lasers.splice(i, 1);
+    } else {
+      ctx.fillStyle = lasers[i].color;
+      lasers[i].position.y -= 2;
+      ctx.fillRect(lasers[i].position.x, lasers[i].position.y, lasers[i].position.w, lasers[i].position.h);
+    }
+  }
+}
+
 function fire() {
-  console.log('FIRE!')
+  let laser = new Laser(hero.position.x + (hero.position.w / 2 - 2));
+  lasers.push(laser);
 };
 
 function gameInit() {
@@ -40,7 +66,10 @@ function gameInit() {
 
 function gameLoop() {
   moveHero();
+  if (lasers.length > 0) {
+    moveLaser();
+  }
   if (playGame === true) {
-    setInterval(gameLoop, 100)
+    setInterval(gameLoop, 100);
   }
 };
