d7cbdb651c2a895bf484467bffc0113b58a1a065
Mike Thomas <chrisandsmith@gmail.com>
2023-01-29 14:45:37 -0600

    Game loop created

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 const gameCanvas = document.querySelector('#game-canvas');
 const ctx = gameCanvas.getContext('2d');
+const playGameBtn = document.querySelector('.start-game');
+var playGame = false;
 
 const hero = {
   color: '#005717',
@@ -11,20 +13,34 @@ const hero = {
   }
 }
 
+playGameBtn.addEventListener('click', () => {
+  playGame = true;
+  gameInit();
+}, false)
+
 document.addEventListener('keydown', (e) => {
   if (e.key == 'ArrowLeft' && hero.position.x > 0) hero.position.x -= 10;
   if (e.key == 'ArrowRight' && hero.position.x < 1230) hero.position.x += 10;
-  moveHero();
+  if (e.key == ' ') fire();
 });
 
 function moveHero() {
   ctx.clearRect(0,0,1280,720);
   ctx.fillStyle = hero.color;
   ctx.fillRect(hero.position.x, hero.position.y, hero.position.w, hero.position.h)
-}
+};
+
+function fire() {
+  console.log('FIRE!')
+};
 
 function gameInit() {
-  moveHero();
-}
+  gameLoop();
+};
 
-gameInit();
+function gameLoop() {
+  moveHero();
+  if (playGame === true) {
+    setInterval(gameLoop, 100)
+  }
+};
